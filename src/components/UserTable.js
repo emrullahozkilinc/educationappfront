32444e579e6bb00356c1c68b8c28dca0a02fb80c
Emrullah <emr.ozk.71@gmail.com>
2022-05-13 17:32:54 +0300

    Edit user template added. Some issues not fixed.

--- src/components/UserTable.js
+++ src/components/UserTable.js
@@ -5,7 +5,15 @@ class UserTable extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            users: []
+            users: [],
+            inputRowId: -1,
+            changedUser: {
+                id: "",
+                name: "",
+                username: "",
+                password: "",
+                type: ""
+            }
         }
     }
 
@@ -22,27 +30,47 @@ class UserTable extends React.Component {
             });
     }
 
+    componentDidUpdate() {
+        axios.get("http://localhost:8080/getUsers", {
+            headers: {
+                "authorization": this.props.authToken
+            }
+        }).then(res => {
+                this.setState({
+                    users: res.data
+                })
+            });
+    }
 
     delClick = (param) => {
-        fetch('http://localhost:8080/delUser/' + param, {
-            method: 'DELETE',
+        axios.delete('http://localhost:8080/delUser/' + param, {
             headers: {
                 'authorization': this.props.authToken
             }
-        })
-            .then(res => {
-                res.json()
-                    .then(data => {
-                        console.log('http://localhost:8080/delUser/' + param);
-                    })
-            });
+        }).then(res => {
+            alert(res.data);
+        });
+    }
+
+    saveSubmit = (param) => {
+        /*
+        axios.put('http://localhost:8080/delUser/' + param, {
+            headers: {'authorization': this.props.authToken},
+            data: {
+
+            }
+        }).then(res => {
+            alert(res.data);
+        });
+         */
+        console.log(this.state.changedUser);
     }
 
 
 
     render() {
         return (
-            <div className="container-sm">
+            <div className="container">
                 <h1 className="text-center">User Table</h1>
                 <table className="table table-striped">
                     <thead>
@@ -56,18 +84,65 @@ class UserTable extends React.Component {
                     </tr>
                     </thead>
                     <tbody>
-                    {this.state.users.map(x =>
-                        <tr key={x.id}>
-                            <td>{x.id}</td>
-                            <td>{x.name}</td>
-                            <td>{x.username}</td>
-                            <td>{x.password}</td>
-                            <td>{x.type}</td>
-                            <td>
-                                <button className="btn btn-primary">Edit</button>
-                                <button className="btn btn-danger" onClick={() => this.delClick(x.id)}>Delete</button>
-                            </td>
-                        </tr>
+                    {this.state.users.map(x => {if (x.id !== this.state.inputRowId) {
+                        return (
+                            <tr key={x.id}>
+                                <td>{x.id}</td>
+                                <td>{x.name}</td>
+                                <td>{x.username}</td>
+                                <td>{x.password}</td>
+                                <td>{x.type}</td>
+                                <td>
+                                    <button className="btn btn-primary" onClick={() => this.setState({...this.state, inputRowId: x.id})}>Edit</button>
+                                    <button className="btn btn-danger" onClick={() => this.delClick(x.id)}>Delete</button>
+                                </td>
+                            </tr>
+                        );
+                    } else {
+                        return (
+                            <tr key={x.id}>
+                                <td hidden={true}><form id="form1" onSubmit={this.saveSubmit }></form></td>
+                                <td><input form="form1" placeholder={x.id} onChange={() =>
+                                    this.setState(
+                                        {...this.state, changedUser:{
+                                            ...this.state.changedUser,
+                                                id:x.id
+                                        }}
+                                    )} className="form-control-sm"/></td>
+                                <td><input form="form1" placeholder={x.name} onChange={() =>
+                                    this.setState(
+                                        {...this.state, changedUser:{
+                                                ...this.state.changedUser,
+                                                name:x.name
+                                            }}
+                                    )} className="form-control-sm"/></td>
+                                <td><input form="form1" placeholder={x.username} onChange={() =>
+                                    this.setState(
+                                        {...this.state, changedUser:{
+                                                ...this.state.changedUser,
+                                                username:x.username
+                                            }}
+                                    )} className="form-control-sm"/></td>
+                                <td><input form="form1" placeholder={x.password} onChange={() =>
+                                    this.setState(
+                                        {...this.state, changedUser:{
+                                                ...this.state.changedUser,
+                                                password:x.password
+                                            }}
+                                    )} className="form-control-sm"/></td>
+                                <td><input form="form1" placeholder={x.type} onChange={() =>
+                                    this.setState(
+                                        {...this.state, changedUser:{
+                                                ...this.state.changedUser,
+                                                type:x.type
+                                            }}
+                                    )} className="form-control-sm"/></td>
+                                <td><input form="form1" type="submit" className="btn btn-success" value="Save"/></td>
+                            </tr>
+                        );
+                        }
+                    }
+
                     )}
                     </tbody>
                 </table>
